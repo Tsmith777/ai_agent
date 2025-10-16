dd83a5e9d83b8b1313ef943aa411edf5670eae68
Tsmith777 <Tsmith777@ymail.com>
2025-10-16 09:27:08 -0700

    Add main.py and tests.py

--- main.py
+++ main.py
@@ -3,25 +3,40 @@ import sys
 from dotenv import load_dotenv
 
 def main():
-    if len(sys.argv) == 1:
+    args = sys.argv[1:]
+    verbose = False
+    if "--verbose" in args:
+        verbose = True
+        args.remove("--verbose")
+
+    if len(args) > 0:
+        user_prompt = args[0]
+    else:
         print("Error: missing prompt. Usage: uv run main.py \"your prompt here\"", file =sys.stderr)
         sys.exit(1)
-
-    prompt = sys.argv[1]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     from google import genai
+    from google.genai import types
+
     client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     resp = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents= prompt,
+        contents= messages,
     )
     print(resp.text)
-    print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
