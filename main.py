eae596d8b998be08b1253b928f83a727a5504efc
Tsmith777 <Tsmith777@ymail.com>
2025-10-30 17:00:02 -0700

    feat(schema): add schema_get_files_info and schema_get_file_content

--- main.py
+++ main.py
@@ -1,46 +1,61 @@
-import os
 import sys
+import os
+from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
+from prompts import system_prompt
+from call_function import available_functions
+
+
 def main():
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
-    model_name = "gemini-2.0-flash-001"
+    load_dotenv()
 
-    args = sys.argv[1:]
-    verbose = False
-    if "--verbose" in args:
-        verbose = True
-        args.remove("--verbose")
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
-    if len(args) > 0:
-        user_prompt = args[0]
-    else:
-        print("Error: missing prompt. Usage: uv run main.py \"your prompt here\"", file =sys.stderr)
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
 
-    load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
-    from google import genai
-    from google.genai import types
-
     client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    response = client.models.generate_content(
-        model = model_name,
-        contents = messages,
-        config = types.GenerateContentConfig(system_instruction=system_prompt),
-    )
-    print(response.text)
+    generate_content(client, messages, verbose)
 
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
     if verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
 
 if __name__ == "__main__":
     main()
