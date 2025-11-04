6c8ba1af29745ced9568122cc5679ea8ddd03c3b
Tsmith777 <Tsmith777@ymail.com>
2025-11-03 22:18:39 -0800

    feat(schema): add schema_run_python_file and schema_write_file

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,6 @@
 import os, sys
 import subprocess
+from google.genai import types
 
 def run_python_file(working_directory, file_path, args=[]):
     
@@ -34,3 +35,18 @@ def run_python_file(working_directory, file_path, args=[]):
 
     except Exception as e:
         return f"Error: executing Python file: {e}"
+    
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Run a python file.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to run, relative to the working directory.",
+            ),
+        },
+        required=["file_path"]
+    ),
+)
--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def write_file(working_directory, file_path, content):
 
@@ -21,3 +22,22 @@ def write_file(working_directory, file_path, content):
         return f"Error: {e}"
     
     return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Write to a file.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to write to, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file.",
+            ),
+        },
+        required=["file_path","content"]
+    ),
+)
